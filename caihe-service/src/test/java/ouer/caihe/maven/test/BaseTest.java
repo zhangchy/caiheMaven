204360853f08f292431ce16e73cccd5191e190bb
caihe <caihe@inxiaopu.com>
2015-10-26 09:43:56 +0800

    添加测试用例

---
+++ caihe-service/src/test/java/ouer/caihe/maven/test/BaseTest.java
@@ -0,0 +1,28 @@
+package ouer.caihe.maven.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ouer.caihe.maven.config.ApplicationConfig;
+import ouer.caihe.maven.config.ServiceConfig;
+
+
+@ActiveProfiles("dev")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ApplicationConfig.class, ServiceConfig.class})
+public abstract class BaseTest<T> {
+
+    protected T provider;
+
+    protected T getProvider() {
+        return provider;
+    }
+
+    @Autowired
+    protected void setProvider(T provider) {
+        this.provider = provider;
+    }
+}
---
+++ caihe-service/src/test/java/ouer/caihe/maven/test/ServiceTest.java
@@ -0,0 +1,12 @@
+package ouer.caihe.maven.test;
+
+import org.junit.Test;
+
+import ouer.caihe.maven.user.service.UserService;
+
+public class ServiceTest extends BaseTest<UserService>{
+	@Test
+	public void serviceTest(){
+		System.out.println("===================================");
+	}
+}
